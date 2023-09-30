7407b9533d1291c7c3f1b2569c8b36b92c42e513
Giovanne Pagano <giovanne3282@gmail.com>
2023-09-29 23:40:12 -0300

    update

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,6 @@
 const ContainerLeft = document.getElementById('containerLeft')
 const LContainerLeft = document.getElementById('littleContainerLeft');
 
-
 const ContainerCenter = document.getElementById('containerCenter')
 const LContainerCenter = document.getElementById('littleContainerCenter');
 
@@ -14,6 +13,14 @@ const BRadius = window.getComputedStyle(LContainerLeft).getPropertyValue('border
 const Linha1 = document.getElementById('linhaInfo1');
 const Linha2 = document.getElementById('linhaInfo2');
 const Linha3 = document.getElementById('linhaInfo3');
+
+const Frame = document.getElementById('containerFrame');
+const Frame2 = document.getElementById('containerFrame2');
+
+
+const FrameSpan = document.getElementById('FrameSpan');
+const FrameText = document.getElementById('FrameText');
+const phrase = ['Confiança', 'Qualidade', 'Transparência'];
 
 ContainerLeft.addEventListener('mouseover', function () {
     LContainerLeft.style.width = '100%';
@@ -47,3 +54,43 @@ ContainerRight.addEventListener('mouseleave', function () {
     LContainerRight.style.borderRadius = BRadius;
     Linha3.style.display = 'block';
 });
+let phraseIndice = 0;
+let indice = 0;
+
+
+function adicionarLetraAoSaida() {
+    if (indice < phrase[phraseIndice].length) {
+        FrameSpan.innerHTML += phrase[phraseIndice][indice];
+        indice++;
+        if (phraseIndice == 0) {
+
+            Frame.style.background = '#2BAFA2';
+            Frame2.style.background = '#209489';
+        }
+        else if (phraseIndice == 1) {
+
+            Frame.style.background = '#73C4F1';
+            Frame2.style.background = '#66b1da';
+        } else {
+            if (phraseIndice == 2) {
+
+                Frame.style.background = '#417AB1';
+                Frame2.style.background = '#3a6fa1';
+            }
+        }
+        setTimeout(adicionarLetraAoSaida, 165);
+    } else {
+        setTimeout(function () {
+            indice = 0;
+            phraseIndice++;
+            if (phraseIndice >= phrase.length) {
+                phraseIndice = 0;
+            }
+            FrameSpan.innerHTML = '';
+            adicionarLetraAoSaida();
+        }, 1200);
+    }
+}
+
+adicionarLetraAoSaida();
+
