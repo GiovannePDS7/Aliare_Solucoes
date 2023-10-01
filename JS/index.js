caad02d11316c9660d6f80489563cb3c2916bc2d
Giovanne Pagano <giovanne3282@gmail.com>
2023-09-30 23:36:23 -0300

    update home

--- JS/index.js
+++ JS/index.js
@@ -57,40 +57,52 @@ ContainerRight.addEventListener('mouseleave', function () {
 let phraseIndice = 0;
 let indice = 0;
 
+const toggleElement = document.getElementById("toggleElement");
+function toggle() {
+    if (toggleElement.style.visibility === "hidden" || toggleElement.style.visibility === "") {
+        toggleElement.style.visibility = "visible";
+    } else {
+        toggleElement.style.visibility = "hidden";
+    }
+}
+function zerarIndice() {
+    toggleElement.style.visibility = "visible";
+    if (indice > 0) {
+        indice--;
+        FrameSpan.innerHTML = FrameSpan.innerHTML.slice(0, -1);
+        setTimeout(zerarIndice, 120);
+    }
+    else {
+        addTextFrame();
+    }
+}
 
-function adicionarLetraAoSaida() {
+function addTextFrame() {
     if (indice < phrase[phraseIndice].length) {
+        toggleElement.style.visibility = "visible";
         FrameSpan.innerHTML += phrase[phraseIndice][indice];
         indice++;
         if (phraseIndice == 0) {
-
             Frame.style.background = '#2BAFA2';
             Frame2.style.background = '#209489';
-        }
-        else if (phraseIndice == 1) {
-
+        } else if (phraseIndice == 1) {
             Frame.style.background = '#73C4F1';
             Frame2.style.background = '#66b1da';
-        } else {
-            if (phraseIndice == 2) {
-
-                Frame.style.background = '#417AB1';
-                Frame2.style.background = '#3a6fa1';
-            }
+        } else if (phraseIndice == 2) {
+            Frame.style.background = '#417AB1';
+            Frame2.style.background = '#3a6fa1';
         }
-        setTimeout(adicionarLetraAoSaida, 165);
+        setTimeout(addTextFrame, 120);
     } else {
+        const toggleInterval = setInterval(toggle, 500);
         setTimeout(function () {
-            indice = 0;
+            zerarIndice()
+            clearInterval(toggleInterval);
             phraseIndice++;
             if (phraseIndice >= phrase.length) {
                 phraseIndice = 0;
             }
-            FrameSpan.innerHTML = '';
-            adicionarLetraAoSaida();
-        }, 1200);
+        }, 2300);
     }
 }
-
-adicionarLetraAoSaida();
-
+addTextFrame();
