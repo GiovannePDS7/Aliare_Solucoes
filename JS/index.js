ed4f50df1c2322f4ee4680000fda410739ca4598
Giovanne Pagano <giovanne3282@gmail.com>
2023-12-15 17:03:40 -0300

    responsividade

--- JS/index.js
+++ JS/index.js
@@ -59,3 +59,30 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+
+let currentIndex = 0;
+
+function showSlide(index) {
+    const carousel = document.querySelector('.carousel');
+    const slides = document.querySelectorAll('.carousel-slide');
+    const totalSlides = slides.length;
+
+    if (index < 0) {
+        currentIndex = totalSlides - 1;
+    } else if (index >= totalSlides) {
+        currentIndex = 0;
+    } else {
+        currentIndex = index;
+    }
+
+    const translateValue = -currentIndex * 100 + '%';
+    carousel.style.transform = 'translateX(' + translateValue + ')';
+}
+
+function nextSlide() {
+    showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+    showSlide(currentIndex - 1);
+}
