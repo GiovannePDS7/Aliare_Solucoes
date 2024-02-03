36ce8e8dfafa2752303f5a293dcdacc318046bea
Giovanne Pagano <giovanne3282@gmail.com>
2024-02-03 20:59:20 -0300

    responsividade

--- JS/index.js
+++ JS/index.js
@@ -1,52 +1,50 @@
-if (window.innerWidth >= 1025) {
-    const Frame = document.getElementById('containerFrame');
-    const Frame2 = document.getElementById('containerFrame2');
+const FrameSpan = document.getElementById('FrameSpan');
+const FrameText = document.getElementById('FrameText');
+const phrase = ['Confiança', 'Qualidade', 'Transparência'];
+let phraseIndice = 0;
+let indice = 0;
 
-    const FrameSpan = document.getElementById('FrameSpan');
-    const FrameText = document.getElementById('FrameText');
-    const phrase = ['Confiança', 'Qualidade', 'Transparência'];
-    let phraseIndice = 0;
-    let indice = 0;
-
-    const toggleElement = document.getElementById("toggleElement");
-    function toggle() {
-        if (toggleElement.style.visibility === "hidden" || toggleElement.style.visibility === "") {
-            toggleElement.style.visibility = "visible";
-        } else {
-            toggleElement.style.visibility = "hidden";
-        }
-    }
-    function zerarIndice() {
+const toggleElement = document.getElementById("toggleElement");
+function toggle() {
+    if (toggleElement.style.visibility === "hidden" || toggleElement.style.visibility === "") {
         toggleElement.style.visibility = "visible";
-        if (indice > 0) {
-            indice--;
-            FrameSpan.innerHTML = FrameSpan.innerHTML.slice(0, -1);
-            setTimeout(zerarIndice, 120);
-        }
-        else {
-            addTextFrame();
-        }
+    } else {
+        toggleElement.style.visibility = "hidden";
     }
+}
+function zerarIndice() {
+    toggleElement.style.visibility = "visible";
+    if (indice > 0) {
+        indice--;
+        FrameSpan.innerHTML = FrameSpan.innerHTML.slice(0, -1);
+        setTimeout(zerarIndice, 120);
+    }
+    else {
+        addTextFrame();
+    }
+}
 
-    function addTextFrame() {
-        if (indice < phrase[phraseIndice].length) {
-            toggleElement.style.visibility = "visible";
-            FrameSpan.innerHTML += phrase[phraseIndice][indice];
-            indice++;
-            setTimeout(addTextFrame, 120);
-        } else {
-            const toggleInterval = setInterval(toggle, 500);
-            setTimeout(function () {
-                zerarIndice()
-                clearInterval(toggleInterval);
-                phraseIndice++;
-                if (phraseIndice >= phrase.length) {
-                    phraseIndice = 0;
-                }
-            }, 2300);
-        }
+function addTextFrame() {
+    if (indice < phrase[phraseIndice].length) {
+        toggleElement.style.visibility = "visible";
+        FrameSpan.innerHTML += phrase[phraseIndice][indice];
+        indice++;
+        setTimeout(addTextFrame, 120);
+    } else {
+        const toggleInterval = setInterval(toggle, 500);
+        setTimeout(function () {
+            zerarIndice()
+            clearInterval(toggleInterval);
+            phraseIndice++;
+            if (phraseIndice >= phrase.length) {
+                phraseIndice = 0;
+            }
+        }, 2300);
     }
-    addTextFrame();
+}
+addTextFrame();
+
+if (window.innerWidth > 1024) {
 
     const header = document.querySelector('header');
     const headerHeight = header.offsetHeight;
@@ -88,96 +86,94 @@ if (window.innerWidth >= 1025) {
             window.scrollBy(0, -headerHeight + 1);
         });
     });
-}
-
-let currentIndex = 0;
-
-function showSlide(index) {
-    const carousel = document.querySelector('.carousel');
-    const slides = document.querySelectorAll('.carousel-slide');
-    const totalSlides = slides.length;
-
-    if (index < 0) {
-        currentIndex = totalSlides - 1;
-    } else if (index >= totalSlides) {
-        currentIndex = 0;
-    } else {
-        currentIndex = index;
-    }
-
-    const translateValue = -currentIndex * 100 + '%';
-    carousel.style.transform = 'translateX(' + translateValue + ')';
-}
-
-function nextSlide() {
-    showSlide(currentIndex + 1);
-}
-
-function prevSlide() {
-    showSlide(currentIndex - 1);
-}
 
 
-const ContainerLeft = document.getElementById('containerLeft')
-const LContainerLeft = document.getElementById('littleContainerLeft');
+    const ContainerLeft = document.getElementById('containerLeft')
+    const LContainerLeft = document.getElementById('littleContainerLeft');
 
-const ContainerCenter = document.getElementById('containerCenter')
-const LContainerCenter = document.getElementById('littleContainerCenter');
+    const ContainerCenter = document.getElementById('containerCenter')
+    const LContainerCenter = document.getElementById('littleContainerCenter');
 
-const ContainerRight = document.getElementById('containerRight')
-const LContainerRight = document.getElementById('littleContainerRight');
+    const ContainerRight = document.getElementById('containerRight')
+    const LContainerRight = document.getElementById('littleContainerRight');
 
-const width = window.getComputedStyle(LContainerLeft).getPropertyValue('width', 10)
-const BRadius = window.getComputedStyle(LContainerLeft).getPropertyValue('border-radius')
+    const width = window.getComputedStyle(LContainerLeft).getPropertyValue('width', 10)
+    const BRadius = window.getComputedStyle(LContainerLeft).getPropertyValue('border-radius')
 
-const Linha1 = document.getElementById('linhaInfo1');
-const Linha2 = document.getElementById('linhaInfo2');
-const Linha3 = document.getElementById('linhaInfo3');
+    const Linha1 = document.getElementById('linhaInfo1');
+    const Linha2 = document.getElementById('linhaInfo2');
+    const Linha3 = document.getElementById('linhaInfo3');
 
-let widthScreen = window.innerWidth;
+    ContainerLeft.addEventListener('mouseover', function () {
 
-ContainerLeft.addEventListener('mouseover', function () {
-    if (widthScreen > 690) {
         LContainerLeft.style.width = '100%';
         LContainerLeft.style.borderRadius = '2%';
         Linha1.style.background = '#000';
-    }
-});
-ContainerLeft.addEventListener('mouseleave', function () {
-    if (widthScreen > 690) {
+
+    });
+    ContainerLeft.addEventListener('mouseleave', function () {
+
         LContainerLeft.style.width = width;
         LContainerLeft.style.borderRadius = BRadius;
         Linha1.style.background = 'linear-gradient(to bottom, #30B0A4, #46D1C4, #57D8CC, #57E6D9)'
-    }
-});
 
-ContainerCenter.addEventListener('mouseover', function () {
-    if (widthScreen > 690) {
+    });
+
+    ContainerCenter.addEventListener('mouseover', function () {
+
         LContainerCenter.style.width = '100%';
         LContainerCenter.style.borderRadius = '2%';
         Linha2.style.background = '#000';
-    }
-});
-ContainerCenter.addEventListener('mouseleave', function () {
-    if (widthScreen > 690) {
+
+    });
+    ContainerCenter.addEventListener('mouseleave', function () {
+
         LContainerCenter.style.width = width;
         LContainerCenter.style.borderRadius = BRadius;
         Linha2.style.background = 'linear-gradient(to bottom, #497BB2, #6495CB, #80AFE1, #88B6E8)';
-    }
-});
 
-ContainerRight.addEventListener('mouseover', function () {
-    if (widthScreen > 690) {
+    });
+
+    ContainerRight.addEventListener('mouseover', function () {
+
         LContainerRight.style.width = '100%';
         LContainerRight.style.borderRadius = '2%';
         Linha3.style.background = '#000';
-    }
-});
-ContainerRight.addEventListener('mouseleave', function () {
-    if (widthScreen > 690) {
+
+    });
+    ContainerRight.addEventListener('mouseleave', function () {
+
         LContainerRight.style.width = width;
         LContainerRight.style.borderRadius = BRadius;
         Linha3.style.background = 'linear-gradient(to bottom, #76C5F0, #82CEF8, #8AD1F8, #9EDDFF)';
-    }
-});
 
+    });
+}
+else {
+    let currentIndex = 0;
+
+    function showSlide(index) {
+        const carousel = document.querySelector('.carousel');
+        const slides = document.querySelectorAll('.carousel-slide');
+        const totalSlides = slides.length;
+
+        if (index < 0) {
+            currentIndex = totalSlides - 1;
+        } else if (index >= totalSlides) {
+            currentIndex = 0;
+        } else {
+            currentIndex = index;
+        }
+
+        const translateValue = -currentIndex * 100 + '%';
+        carousel.style.transform = 'translateX(' + translateValue + ')';
+    }
+
+    function nextSlide() {
+        showSlide(currentIndex + 1);
+    }
+
+    function prevSlide() {
+        showSlide(currentIndex - 1);
+    }
+}
