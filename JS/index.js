3b0af2ce277da92ddacdd68f7bed3f088408c1e0
Giovanne Pagano <giovanne3282@gmail.com>
2024-02-17 16:07:41 -0300

    js update

--- JS/index.js
+++ JS/index.js
@@ -43,136 +43,135 @@ function addTextFrame() {
     }
 }
 addTextFrame();
+if (window.innerWidth > 1024) {
+    const header = document.querySelector('header');
+    const headerHeight = header.offsetHeight;
 
+    const liProjetos = document.getElementById('liProjetos');
+    const liParceiros = document.getElementById('liParceiros');
+    const liQmSomos = document.getElementById('liQmSomos');
+    const projetoSection = document.getElementById('projeto');
+    const parceirosSection = document.getElementById('Parceiros');
+    const qmsomosSection = document.getElementById('QmSomos');
 
+    const liFooterParceiros = document.getElementById('liFooterParceiros');
+    const liFooterProjetos = document.getElementById('liFooterProjetos');
+    const liFooterQmSomos = document.getElementById('liFooterQmSomos');
 
-const header = document.querySelector('header');
-const headerHeight = header.offsetHeight;
-
-const liProjetos = document.getElementById('liProjetos');
-const liParceiros = document.getElementById('liParceiros');
-const liQmSomos = document.getElementById('liQmSomos');
-const projetoSection = document.getElementById('projeto');
-const parceirosSection = document.getElementById('Parceiros');
-const qmsomosSection = document.getElementById('QmSomos');
-
-const liFooterParceiros = document.getElementById('liFooterParceiros');
-const liFooterProjetos = document.getElementById('liFooterProjetos');
-const liFooterQmSomos = document.getElementById('liFooterQmSomos');
-
-document.addEventListener('DOMContentLoaded', function () {
-    liParceiros.addEventListener('click', function () {
-        parceirosSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        window.scrollBy(0, -headerHeight + 1);
+    document.addEventListener('DOMContentLoaded', function () {
+        liParceiros.addEventListener('click', function () {
+            parceirosSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            window.scrollBy(0, -headerHeight + 1);
+        });
+        liProjetos.addEventListener('click', function () {
+            projetoSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            window.scrollBy(0, -headerHeight + 1);
+        });
+        liQmSomos.addEventListener('click', function () {
+            qmsomosSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            window.scrollBy(0, -headerHeight + 1);
+        });
+        liFooterParceiros.addEventListener('click', function () {
+            parceirosSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            window.scrollBy(0, -headerHeight + 1);
+        });
+        liFooterProjetos.addEventListener('click', function () {
+            projetoSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            window.scrollBy(0, -headerHeight + 1);
+        });
+        liFooterQmSomos.addEventListener('click', function () {
+            qmsomosSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            window.scrollBy(0, -headerHeight + 1);
+        });
     });
-    liProjetos.addEventListener('click', function () {
-        projetoSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        window.scrollBy(0, -headerHeight + 1);
+
+
+    const ContainerLeft = document.getElementById('containerLeft');
+    const LContainerLeft = document.getElementById('littleContainerLeft');
+
+    const ContainerCenter = document.getElementById('containerCenter')
+    const LContainerCenter = document.getElementById('littleContainerCenter');
+
+    const ContainerRight = document.getElementById('containerRight')
+    const LContainerRight = document.getElementById('littleContainerRight');
+
+    const width = window.getComputedStyle(LContainerLeft).getPropertyValue('width', 10)
+    const BRadius = window.getComputedStyle(LContainerLeft).getPropertyValue('border-radius')
+
+    const Linha1 = document.getElementById('linhaInfo1');
+    const Linha2 = document.getElementById('linhaInfo2');
+    const Linha3 = document.getElementById('linhaInfo3');
+
+    ContainerLeft.addEventListener('mouseover', function () {
+
+        LContainerLeft.style.width = '100%';
+        LContainerLeft.style.borderRadius = '2%';
+        Linha1.style.background = '#000';
+
     });
-    liQmSomos.addEventListener('click', function () {
-        qmsomosSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        window.scrollBy(0, -headerHeight + 1);
+    ContainerLeft.addEventListener('mouseleave', function () {
+
+        LContainerLeft.style.width = width;
+        LContainerLeft.style.borderRadius = BRadius;
+        Linha1.style.background = 'linear-gradient(to bottom, #30B0A4, #46D1C4, #57D8CC, #57E6D9)'
+
     });
-    liFooterParceiros.addEventListener('click', function () {
-        parceirosSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        window.scrollBy(0, -headerHeight + 1);
+
+    ContainerCenter.addEventListener('mouseover', function () {
+
+        LContainerCenter.style.width = '100%';
+        LContainerCenter.style.borderRadius = '2%';
+        Linha2.style.background = '#000';
+
     });
-    liFooterProjetos.addEventListener('click', function () {
-        projetoSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        window.scrollBy(0, -headerHeight + 1);
+    ContainerCenter.addEventListener('mouseleave', function () {
+
+        LContainerCenter.style.width = width;
+        LContainerCenter.style.borderRadius = BRadius;
+        Linha2.style.background = 'linear-gradient(to bottom, #497BB2, #6495CB, #80AFE1, #88B6E8)';
+
     });
-    liFooterQmSomos.addEventListener('click', function () {
-        qmsomosSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        window.scrollBy(0, -headerHeight + 1);
+
+    ContainerRight.addEventListener('mouseover', function () {
+
+        LContainerRight.style.width = '100%';
+        LContainerRight.style.borderRadius = '2%';
+        Linha3.style.background = '#000';
+
     });
-});
+    ContainerRight.addEventListener('mouseleave', function () {
 
+        LContainerRight.style.width = width;
+        LContainerRight.style.borderRadius = BRadius;
+        Linha3.style.background = 'linear-gradient(to bottom, #76C5F0, #82CEF8, #8AD1F8, #9EDDFF)';
 
-const ContainerLeft = document.getElementById('containerLeft')
-const LContainerLeft = document.getElementById('littleContainerLeft');
+    });
+}
+else {
+    let currentIndex = 0;
 
-const ContainerCenter = document.getElementById('containerCenter')
-const LContainerCenter = document.getElementById('littleContainerCenter');
+    function showSlide(index) {
+        const carousel = document.querySelector('.carousel');
+        const slides = document.querySelectorAll('.carousel-slide');
+        const totalSlides = slides.length;
 
-const ContainerRight = document.getElementById('containerRight')
-const LContainerRight = document.getElementById('littleContainerRight');
+        if (index < 0) {
+            currentIndex = totalSlides - 1;
+        } else if (index >= totalSlides) {
+            currentIndex = 0;
+        } else {
+            currentIndex = index;
+        }
 
-const width = window.getComputedStyle(LContainerLeft).getPropertyValue('width', 10)
-const BRadius = window.getComputedStyle(LContainerLeft).getPropertyValue('border-radius')
-
-const Linha1 = document.getElementById('linhaInfo1');
-const Linha2 = document.getElementById('linhaInfo2');
-const Linha3 = document.getElementById('linhaInfo3');
-
-ContainerLeft.addEventListener('mouseover', function () {
-
-    LContainerLeft.style.width = '100%';
-    LContainerLeft.style.borderRadius = '2%';
-    Linha1.style.background = '#000';
-
-});
-ContainerLeft.addEventListener('mouseleave', function () {
-
-    LContainerLeft.style.width = width;
-    LContainerLeft.style.borderRadius = BRadius;
-    Linha1.style.background = 'linear-gradient(to bottom, #30B0A4, #46D1C4, #57D8CC, #57E6D9)'
-
-});
-
-ContainerCenter.addEventListener('mouseover', function () {
-
-    LContainerCenter.style.width = '100%';
-    LContainerCenter.style.borderRadius = '2%';
-    Linha2.style.background = '#000';
-
-});
-ContainerCenter.addEventListener('mouseleave', function () {
-
-    LContainerCenter.style.width = width;
-    LContainerCenter.style.borderRadius = BRadius;
-    Linha2.style.background = 'linear-gradient(to bottom, #497BB2, #6495CB, #80AFE1, #88B6E8)';
-
-});
-
-ContainerRight.addEventListener('mouseover', function () {
-
-    LContainerRight.style.width = '100%';
-    LContainerRight.style.borderRadius = '2%';
-    Linha3.style.background = '#000';
-
-});
-ContainerRight.addEventListener('mouseleave', function () {
-
-    LContainerRight.style.width = width;
-    LContainerRight.style.borderRadius = BRadius;
-    Linha3.style.background = 'linear-gradient(to bottom, #76C5F0, #82CEF8, #8AD1F8, #9EDDFF)';
-
-});
-
-
-let currentIndex = 0;
-
-function showSlide(index) {
-    const carousel = document.querySelector('.carousel');
-    const slides = document.querySelectorAll('.carousel-slide');
-    const totalSlides = slides.length;
-
-    if (index < 0) {
-        currentIndex = totalSlides - 1;
-    } else if (index >= totalSlides) {
-        currentIndex = 0;
-    } else {
-        currentIndex = index;
+        const translateValue = -currentIndex * 100 + '%';
+        carousel.style.transform = 'translateX(' + translateValue + ')';
     }
 
-    const translateValue = -currentIndex * 100 + '%';
-    carousel.style.transform = 'translateX(' + translateValue + ')';
-}
+    function nextSlide() {
+        showSlide(currentIndex + 1);
+    }
 
-function nextSlide() {
-    showSlide(currentIndex + 1);
-}
-
-function prevSlide() {
-    showSlide(currentIndex - 1);
+    function prevSlide() {
+        showSlide(currentIndex - 1);
+    }
 }
